d36c48c32d593322c0f7e5643f2cefa7af29ed52
git status <215340863+g-sts@users.noreply.github.com>
2025-08-03 02:48:34 -0300

    feat: update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,31 @@
+const dogButton = document.getElementById('btn-dog')
+const catButton = document.getElementById('btn-cat')
+const surpriseButton = document.getElementById('btn-surprise')
+const img = document.querySelector('img')
+const p = document.querySelector('p');
 
+dogButton.addEventListener('click', ()=>{
+  fetch('https://dog.ceo/api/breeds/image/random')
+    .then(response => response.json())
+    .then(data =>{
+      img.src = data.message;
+      img.style.display = 'block';
+      p.style.display = 'none';
+    })
+    .catch(error =>{
+      console.error('Erro ao buscar imagem:', error)
+    });
+});
+
+catButton.addEventListener('click', () =>{
+  fetch('https://api.thecatapi.com/v1/images/search')
+    .then(response => response.json())
+    .then(data => {
+      img.src = data[0].url;
+      img.style.display = 'block';
+      p.style.display = 'none';
+    })
+    .catch(error =>{
+      console.error('Erro ao buscar imagem de gato', error);
+    });
+});
